6910ef76fb3636e1fe48388692fba5e77c1c254a
Stephanie Acevedo <stephanie.dominguez@centretek.com>
2021-02-25 17:19:14 -0500

    Toggle for dark mode and alt text for portfolio images

--- src/js/global.js
+++ src/js/global.js
@@ -40,9 +40,26 @@ window.onload = function() {
       link.addEventListener('click', toggleNav);
   });
   
+  // Dark Mode
+  let checkbox = document.querySelector('input[name=theme]');
 
+  checkbox.addEventListener('change', function() {
+      if(this.checked) {
+          trans()
+          document.documentElement.setAttribute('data-theme', 'dark')
+      } else {
+          trans()
+          document.documentElement.setAttribute('data-theme', 'light')
+      }
+  })
+
+  let trans = () => {
+      document.documentElement.classList.add('transition');
+      window.setTimeout(() => {
+          document.documentElement.classList.remove('transition')
+      }, 1000)
+  }
   
-
   // Swiper slider
   const swiper = new Swiper('.swiper-container', {
     // Optional parameters
